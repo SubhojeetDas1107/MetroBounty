0a3e5c8583d9adc28400e1eca6b8df1126603729
Subhojeet <hbksubhojeetdas1107@gmail.com>
2021-09-26 07:22:07 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+let weather = {
+    "apiKey": "2edb891504a4aba85d525f7a119893c4",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=metric&appid=" 
+        + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity} = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerHTML = "Weather in " + name;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
+        document.querySelector(".description").innerText = "Description: " + description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h"; 
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
+})
